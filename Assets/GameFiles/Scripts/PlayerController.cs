aed7aca617953d2980b211a0442141fb995b1075
TaylorDevMAIN <taylorwoodallpriority@gmail.com>
2025-06-12 20:59:43 +0100

    Added game files
    Added the game files and set up repo!

---
+++ Assets/GameFiles/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float defaultspeed = 10;
+    public bool isTouchingWater = false;
+
+    private float walkspeed;
+    private CharacterController controller;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        walkspeed = defaultspeed;
+    }
+
+    void Update()
+    {
+    if (Input.GetMouseButton(1))
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        this.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    Vector3 movement = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    movement = transform.rotation * movement * Time.deltaTime * walkspeed;
+    controller.Move(movement);
+
+    if (Input.GetKey(KeyCode.LeftShift) && walkspeed == 10)
+    {
+        walkspeed = walkspeed * 2;
+    } else {
+        walkspeed = defaultspeed;
+    }
+    }
+}
